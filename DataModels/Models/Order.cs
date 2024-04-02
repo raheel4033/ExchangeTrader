4fed470cf602333ab366b266a7ace292b70e622f
raheel4033 <raheelh717@hotmail.com>
2024-04-02 23:38:05 +0500

    Models Added

---
+++ DataModels/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int productID { get; set; }
+        public long price { get; set; }
+        public long  quantity { get; set; }
+        public string? side { get; set; }
+        public string? orderStatus { get; set; }
+        public string? orderType { get; set; }
+        public string? timeInForce { get; set; }
+        public string? symbol { get; set; }
+        public long? userId { get; set; }
+        public string? clientOrderId { get; set; }
+        public string? broker { get; set;}
+        public long accountID { get; set; }
+        public long entryTime { get; set; }
+        public long transactionTime { get; set; }
+
+
+
+    }
+}
