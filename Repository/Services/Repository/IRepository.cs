487f326fe637bafbe5a9731666afc21feceae217
ali.99asim <ali.99asim@gmail.com>
2024-04-09 00:26:08 +0500

    Added Services Layet to repository pattern

--- Repository/Services/Repository/IRepository.cs
+++ Repository/Services/Repository/IRepository.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 
 namespace DataModels.Repository.IRepository
 {
-    public interface IRepository<T> where T:class
+    public interface IRepository<T> where T :class
     {
+        T GetById(int id);
         IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
-        void Remove(T entity);
-        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void Delete(T entity);
     }
+
 }
---
+++ Repository/Services/Repository/OrderRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataModels.Data;
+using DataModels.Models;
+using DataModels.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Services.Repository
+{
+    public class OrderRepository : IRepository<Order>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Order GetById(int id)
+        {
+            return _dbContext.Orders.Find(id);
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _dbContext.Orders.ToList();
+        }
+
+        public void Add(Order entity)
+        {
+            _dbContext.Orders.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Order entity)
+        {
+            _dbContext.Orders.Update(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(Order entity)
+        {
+            _dbContext.Orders.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
